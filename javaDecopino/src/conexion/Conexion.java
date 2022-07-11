9ca54215fde0352241c912cd7a1bdbbba1669ab5
hagi11 <hammerhagi5@gmail.com>
2022-07-10 19:54:39 -0500

    Documento inicial

---
+++ javaDecopino/src/conexion/Conexion.java
@@ -0,0 +1,70 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion implements Configuracion {
+
+    private static Connection con = null;
+    
+    static {
+        try {
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+            if (con != null) {
+                System.out.println("Conexion exitosa");
+            } else {
+                System.out.println("Conexion fallida");
+            }
+        } catch (ClassNotFoundException | SQLException error) {
+            error.printStackTrace();
+            System.out.println("Error en conexión: "+error);
+        }
+    }
+    
+    public static Connection getConnection() {
+        return con;
+    }
+    
+    public static void cerrar() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Error en cerrar : "+e);
+        }
+         
+    } 
+
+    public boolean ejecutar(String sql) {
+        boolean var;
+        try {
+            Statement sentencia;
+            sentencia = getConnection().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            if(sentencia.executeUpdate(sql)==0)
+                var=false;
+            else            
+                var = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error en ejecución : "+e);
+            var = false;
+        }
+        return var;        
+    }
+
+    public ResultSet consultar(String sql) {
+        ResultSet resultado=null;
+        try {
+            Statement sentencia;
+            sentencia = getConnection().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            resultado = sentencia.executeQuery(sql); 
+        } catch (SQLException error) {
+            error.printStackTrace();
+            System.out.println("Error en la consulta : "+error);
+        }
+        return resultado;
+    }
+}
---
+++ javaDecopino/src/conexion/Configuracion.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface Configuracion {
+
+//    String DRIVER = "oracle.jdbc.driver.OracleDriver";
+//    String CONNECTION_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+//    String USERNAME = "system";
+//    String PASSWORD = "oracle";
+    
+//    String DRIVER = "com.mysql.jdbc.Driver";
+//    String DATA_BASE = "PruebaADSI194";
+//    String CONNECTION_URL = "jdbc:mysql://localhost:3306/"+DATA_BASE;    
+//    String USERNAME = "root";
+//    String PASSWORD = "";
+    
+//    String DRIVER = "com.mysql.jdbc.Driver";
+//    String DATA_BASE = "softclotech_ADSI192_prueba";   
+//    String CONNECTION_URL = "jdbc:mysql://34.95.206.133:3306/"+DATA_BASE;
+//    String USERNAME = "softclotech_invitadoadsi192";
+//    String PASSWORD = "Adsi192invitado";
+    
+//    String DRIVER = "com.mysql.jdbc.Driver";
+//    String DATA_BASE = "yobusco_165-Adsi";
+//    String CONNECTION_URL = "jdbc:mysql://158.85.67.158:3306/"+DATA_BASE;    
+//    String USERNAME = "yobusco_165-invi";
+//    String PASSWORD = "adsi1234567*165";
+    
+//    String DRIVER = "org.postgresql.Driver";
+//    String DATA_BASE = "postgres";
+//    String CONNECTION_URL = "jdbc:postgresql://localhost:5432/"+DATA_BASE;    
+//    String USERNAME = "postgres";
+//    String PASSWORD = "jaime";
+//    
+    String DRIVER = "org.apache.derby.jdbc.ClientDriver";
+    String DATA_BASE = "agenda";
+    String CONNECTION_URL = "jdbc:derby://localhost:1527/"+DATA_BASE;    
+    String USERNAME = "agenda";
+    String PASSWORD = "agenda";
+}
+
